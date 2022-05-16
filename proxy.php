c65316cc298186bc77343df63c3f01b36d0e5655
Woodfish <86752391+thewoodfish@users.noreply.github.com>
2022-05-16 21:20:55 +0000

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,104 @@
+<?php
+$enable_jsonp    = false;
+$enable_native   = false;
+$valid_url_regex = '/.*/';
+
+// ############################################################################
+
+$url = $_GET['url'];
+
+if ( !$url ) {
+  
+  // Passed url not specified.
+  $contents = 'ERROR: url not specified';
+  $status = array( 'http_code' => 'ERROR' );
+  
+} else if ( !preg_match( $valid_url_regex, $url ) ) {
+  
+  // Passed url doesn't match $valid_url_regex.
+  $contents = 'ERROR: invalid url';
+  $status = array( 'http_code' => 'ERROR' );
+  
+} else {
+  echo "it went through";
+  $ch = curl_init( $url );
+  
+  if ( strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $_POST );
+  }
+  
+  if ( $_GET['send_cookies'] ) {
+    $cookie = array();
+    foreach ( $_COOKIE as $key => $value ) {
+      $cookie[] = $key . '=' . $value;
+    }
+    if ( $_GET['send_session'] ) {
+      $cookie[] = SID;
+    }
+    $cookie = implode( '; ', $cookie );
+    
+    curl_setopt( $ch, CURLOPT_COOKIE, $cookie );
+  }
+  
+  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+  curl_setopt( $ch, CURLOPT_HEADER, true );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+  
+  curl_setopt( $ch, CURLOPT_USERAGENT, $_GET['user_agent'] ? $_GET['user_agent'] : $_SERVER['HTTP_USER_AGENT'] );
+  
+  list( $header, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch ), 2 );
+  
+  $status = curl_getinfo( $ch );
+  
+  curl_close( $ch );
+}
+
+echo $contents;
+
+// Split header text into an array.
+$header_text = preg_split( '/[\r\n]+/', $header );
+
+if ( $_GET['mode'] == 'native' ) {
+  if ( !$enable_native ) {
+    $contents = 'ERROR: invalid mode';
+    $status = array( 'http_code' => 'ERROR' );
+  }
+  
+  // Propagate headers to response.
+  foreach ( $header_text as $header ) {
+    if ( preg_match( '/^(?:Content-Type|Content-Language|Set-Cookie):/i', $header ) ) {
+      header( $header );
+    }
+  }
+  
+  echo $contents;
+  
+} else {
+  
+  // $data will be serialized into JSON data.
+  $data = array();
+  
+  // Propagate all HTTP headers into the JSON data object.
+  if ( $_GET['full_headers'] ) {
+    $data['headers'] = array();
+    
+    foreach ( $header_text as $header ) {
+      preg_match( '/^(.+?):\s+(.*)$/', $header, $matches );
+      if ( $matches ) {
+        $data['headers'][ $matches[1] ] = $matches[2];
+      }
+    }
+  }
+  
+  // Propagate all cURL request / response info to the JSON data object.
+  if ( $_GET['full_status'] ) {
+    $data['status'] = $status;
+  } else {
+    $data['status'] = array();
+    $data['status']['http_code'] = $status['http_code'];
+  }
+  
+    echo $decoded_json ? $decoded_json : $contents;
+  
+}
